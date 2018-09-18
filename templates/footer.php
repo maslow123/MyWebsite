49947dae328ad34e90e2d960b60dd5952ccb9b06
maslow123 <maslownr@gmail.com>
2018-09-18 18:55:57 +0700

    first commit

---
+++ templates/footer.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/footer.css">
+</head>
+
+<body>
+	<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+    <div class="footer-dark">
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>Skill</h3>
+                        <ul>
+                            <li><a href="#">Web Design</a></li>
+                            <li><a href="#">Development</a></li>                           
+                        </ul>
+                    </div>
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>About Me</h3>
+                        <ul>
+                            <li><a href="#">Biografi</a></li>
+                            <li><a href="#">Portfolio</a></li>
+                            <li><a href="#">Careers</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6 item text">
+                        <h3>Short Bioghrapy</h3>
+                        <p>Saya adalah seorang pelajar SMK, yang masih haus akan banyak ilmu pengetahuan .
+              			   Dan saya adalah seorang pelajar yang akan terus belajar untuk mengembangkan diri agar
+              			   kelak bisa berguna bagi perubahan dunia.
+                        </p>
+                    </div>
+                    <div class="col item social">
+                    	<a href="https://web.facebook.com/fadhly.rasyaii"><i class="icon ion-social-facebook"></i></a>
+                    	<a href="#"><i class="icon ion-social-twitter"></i></a>
+                    	<a href=""><i class="icon ion-social-whatsapp"></i></a>
+                    	<a href="https://www.instagram.com/fadhlynrrzqi/"><i class="icon ion-social-instagram"></i></a>
+                    </div>
+                </div>
+                <p class="copyright">Muhammad Fadhly Noor Rizqi</p>
+            </div>
+        </footer>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>

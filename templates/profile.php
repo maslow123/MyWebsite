2991d9b1e0e2b2947cffad90bcabfd6f342254c1
maslow123 <maslownr@gmail.com>
2018-09-25 20:25:23 +0700

    modify-sub-and-sosmed-profile

--- templates/profile.php
+++ templates/profile.php
@@ -16,18 +16,51 @@
 	<div class="wrapper">
 		<div class="col-md-2 col-xs-2 p-image">
 			<img src="../assets/images/me.jpg">
+			<p class="name">M.Fadhly NR</p>
+			<p class="job">Junior Developer</p>
 		</div>
 		<div class="col-md-7 p-content">
+			<h2 style="text-align: center;font-family: 'Inconsolata';">MY PROFILE</h2>
 			<div class="w3-panel w3-leftbar w3-light-grey">
       			<h3 class="b-sub">
       				Hello, I'm a junior developer /<div style="color:#1d90fe; float:right; padding-left:3px; font-size:18px; ">Web Developer / Web Design</div>
       			</h3>
       			<h3 class="b-sub-2">
-      				Ini kontentIni kontentIni kontentIni kontentIni kontentIni kontentIni kontentIni kontent
-      				Ini kontentIni kontentIni kontentIni kontentIni kontentIni kontentIni kontentIni kontent
-      				Ini kontentIni kontentIni kontentIni kontentIni kontentIni kontentIni kontentIni kontent
-      				Ini kontentIni kontentIni kontentIni kontentIni kontentIni kontentIni kontentIni kontent
+      				Saya adalah junior programmer yang hanya berpendidikan SMK, mulai mengenali programming
+      				pada saat baru masuk sekolah menengah kejuruan (SMK), hingga akhirnya saya mulai tertarik
+      				dengan website programming language (HTML,CSS,JS,PHP). 
       			</h3>
+    		</div>
+    		<h2 style="text-align:center;font-family: 'Inconsolata';">TEST</h2>
+			<div class="w3-panel w3-leftbar w3-light-grey social">
+				<div class="col-md-3 col-xs-2" >
+					<h3 class="b-sub header">
+						Email : <br/>
+						Phone :	<br/>
+						Date of birth :	<br/>
+					</h3>
+				</div>
+				<div class="col-md-3">
+					<h3 class="b-sub social-media">
+						mfadhlyn@gmail.com <br/>
+						081223674749 <br/>
+						October 29,2001<br/>
+					</h3>
+				</div>
+				<div class="col-md-3 col-xs-2">
+					<h3 class="b-sub header">
+						Address :<br/>
+						Website :<br/>
+						Language:<br/>
+					</h3>
+				</div>
+				<div class="col-md-3">
+					<h3 class="b-sub social-media">
+						Griya Cipeucang Indah, Blok C6/8 <br/>
+						lalala.com<br/>
+						Indonesian<br/>
+					</h3>
+				</div>
     		</div>
 		</div>
 	</div>
